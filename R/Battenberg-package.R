a49e4f72f9b57edbcd6b027261154266a784fe80
Stefan Dentro <sd11@sanger.ac.uk>
2015-11-04 18:47:15 +0000

    Creating BB master R script with some docs included

---
+++ R/Battenberg-package.R
@@ -0,0 +1,7 @@
+#' @name Battenberg
+#' @docType package
+#' @title Battenberg subclonal copy number R Package
+#' @author Stefan Dentro, \email{sd11@@sanger.ac.uk}
+#' @keywords package
+#' @importFrom RColorBrewer brewer.pal
+NA
