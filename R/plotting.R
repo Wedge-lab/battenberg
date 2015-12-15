38fdf7ca1e80af2b44bb9e74a00b3515d6c3511f
Jonas Demeulemeester <zeunas@gmail.com>
2015-12-15 13:38:51 +0000

    making sure we're using psiT and using it in the correct formula to backcalc nA and nB

--- R/plotting.R
+++ R/plotting.R
@@ -292,13 +292,12 @@ squaresplot <- function(tumourname, run_dir, segment_chr, segment_pos, platform_
   segment_pos <- as.numeric(gsub("M", "000000", segment_pos))
   subclones <- read.table(paste(run_dir, tumourname, "_subclones.txt", sep=""), header=T, stringsAsFactors=F)
   subclone <- subclones[(subclones$chr == segment_chr) & (subclones$startpos <= segment_pos) & (subclones$endpos >= segment_pos),]
-  
   rhopsi <- read.table(paste(run_dir, tumourname, "_rho_and_psi.txt", sep=""), header = T, stringsAsFactors=F)
   rhopsi <- rhopsi[which(rhopsi$is.best == TRUE), c("rho", "psi")] 
-  rhopsi$psi <- rhopsi$rho*rhopsi$psi + 2 * (1-rhopsi$rho) # psi of all cells 
   
   nMincalc <- (rhopsi$rho-1-(subclone$BAF-1)*2^(subclone$LogR/platform_gamma)*((1-rhopsi$rho)*2+rhopsi$rho*rhopsi$psi))/rhopsi$rho
   nMajcalc <- (rhopsi$rho-1+subclone$BAF*2^(subclone$LogR/platform_gamma)*((1-rhopsi$rho)*2+rhopsi$rho*rhopsi$psi))/rhopsi$rho
+
   subclone <- data.frame(subclone, rhopsi, nMincalc, nMajcalc)
   
   # helper function to calculate isobaflines
